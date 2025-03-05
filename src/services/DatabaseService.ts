6326f98c243fc00f3bb41ed1501115378d2d2f7d
Invista e Ganhe <lucasalves.analista@hotmail.com>
2025-03-05 14:35:40 -0300

    Update DatabaseService.ts

--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -275,6 +275,20 @@ const DatabaseService: DB = {
           endDate: new Date("2025-09-05").toISOString()
         }
       },
+       {
+            id: "74d77321",
+            amount: 505.05,
+            period: 6,
+            startDate: new Date("2025-03-05").toISOString(),
+            endDate: new Date("2025-09-05").toISOString()
+          },
+ {
+            id: "104e9bbc",
+            amount: 606.06,
+            period: 6,
+            startDate: new Date("2025-03-05").toISOString(),
+            endDate: new Date("2025-09-05").toISOString()
+          }
       {
         name: "Marlene Ayla Alves",
         email: "marlene_ayla_alves303@life.com",
