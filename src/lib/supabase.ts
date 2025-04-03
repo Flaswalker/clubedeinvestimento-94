40ff2906dabf1343215103d074b00c7b6c823632
Invista e Ganhe <lucasalves.analista@hotmail.com>
2025-04-03 13:45:34 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,1 +1,29 @@
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = '';
+const supabaseKey = '';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export type Tables = {
+  users: {
+    name: string;
+    email: string;
+    celular: string;
+    cpf: string;
+    is_admin: boolean;
+    is_verified: boolean;
+  };
+  investments: {
+    id: string;
+    user_email: string;
+    amount: number;
+    period: number;
+    start_date: string;
+    end_date: string;
+  };
+  passwords: {
+    email: string;
+    password: string;
+  };
+};
