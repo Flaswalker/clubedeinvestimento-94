7421bcfb13f7eaf7510e67fcaf2e44369495cf43
Invista e Ganhe <lucasalves.analista@hotmail.com>
2025-04-06 02:19:23 -0300

    Update InvestmentForm.tsx

--- src/components/admin/InvestmentForm.tsx
+++ src/components/admin/InvestmentForm.tsx
@@ -1,10 +1,4 @@
 
-clubedeinvestimento-71/ fonte/ componentes/ administrador
-/Formulário de Investimento.tsx
-
-$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
