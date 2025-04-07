0893f7c682b24bf2550752fa91779acbc8c9cfa1
Invista e Ganhe <lucasalves.analista@hotmail.com>
2025-04-07 20:40:32 -0300

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,4 @@
+
 export interface User {
   name: string;
   email: string;
@@ -15,15 +16,6 @@ export interface Investment {
   period: number; // in months
   startDate: string;
   endDate: string;
-}
-
-export interface WithdrawalRequest {
-  id: string;
-  email: string;
-  valor: number;
-  data: string;
-  status: string;
-  pix: string;
 }
 
 export interface AuthContextType {
