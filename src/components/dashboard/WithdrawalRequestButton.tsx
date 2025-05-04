fd32261f9a3b27b98bb729c9a25591a9bb115b51
Invista e Ganhe <lucasalves.analista@hotmail.com>
2025-05-04 11:08:20 -0300

    Update WithdrawalRequestButton.tsx

--- src/components/dashboard/WithdrawalRequestButton.tsx
+++ src/components/dashboard/WithdrawalRequestButton.tsx
@@ -124,7 +124,7 @@ export default function WithdrawalRequestButton() {
         <button
           type="submit"
           disabled={isLoading || amount <= 100 || !pix}
-          className="w-full py-2 px-4 bg-blue-600 text-white rounded-md disabled:bg-gray-400"
+          className="w-fit py-2 px-4 bg-blue-600 text-white rounded-md disabled:bg-gray-400"
         >
           {isLoading ? "Enviando..." : "Solicitar"}
         </button>
