9ef1606045c127dcd5c2fdbe8de735fef0c0d29c
Invista e Ganhe <lucasalves.analista@hotmail.com>
2026-07-02 15:06:58 -0300

    Clarify investment return and withdrawal conditions
    Updated text for clarity regarding investment terms and conditions.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -201,7 +201,7 @@ const Dashboard = () => {
                           <div className="border p-4 rounded-md bg-slate-50/30">
                             <p className="text-sm leading-6">
                               Estou ciente das condições deste investimento, que{" "}
-                             <span className="font-bold">conferem</span> um retorno aproximado de 15% no semestre (seis meses). O resgate será após 365 dias contados a partir do investimento. O investidor deve entrar no site pelo menos uma vez a cada seis meses para solicitar,{" "}
+                             <span className="font-bold">conferem</span> um retorno aproximado de 15% no semestre (seis meses). O resgate será após 365 dias contados a partir da data do investimento. O investidor deve entrar no site pelo menos uma vez a cada seis meses para solicitar,{" "}
                               <span className="font-bold">através do envio de PROPOSTA,</span> o reinvestimento do valor para mais seis meses e, assim,{" "}
                               <span className="font-bold">lucrar mais de 30% ao ano.</span> Caso esqueça, o valor será corrigido pelos juros da poupança no ano vigente,{" "}
                               <span className="font-bold">conforme estabelecido pelo Banco Central,</span> calculado de forma proporcional ao tempo de investimento.{" "}
